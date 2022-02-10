c6a83446c355de2116a60f0ac0cbab8b98ee6028
GabytaDev <gabyta.loyola@gmail.com>
2022-02-10 17:24:01 -0300

    agrego maquetado y estilos basicos a busqueda

--- src/components/Seccion-principal.js
+++ src/components/Seccion-principal.js
@@ -2,9 +2,17 @@ import "../styles/_SeccionPrincipal.scss";
 
 const SeccionPrincipal = ()=>{
      return (
-        <div>
-            
-        </div>
+        <main>
+           <section className="seccion-busqueda">
+            <form className="form">
+               <input type="text" placeholder="Search" className="input"></input>
+               <select>
+                  <option>Characters</option>
+                  <option>Episodes</option>
+               </select>
+            </form>
+           </section>  
+        </main>
      )
 
 }
