be3f1a05faa07aa7e37f9610ee213acc73a580c4
GabytaDev <gabyta.loyola@gmail.com>
2022-02-11 20:24:04 -0300

    agrego estilos a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
 import "../styles/_Footer.scss"
+import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 const Footer = ()=>{
     return(
         <div>
             <section className="content-footer">
-                <p>Hecho por Gabriela Loyola Carpio</p>
-                <div className="icon"> <GitHubIcon/></div>
-                <div className="icon"> <MailOutlineIcon/></div>
-
+                <Typography variant="h6">Hecho por Gabriela Loyola Carpio</Typography>
+                <div className="content-icons">
+                <div className="icon"> <GitHubIcon sx={{ fontSize: 40 }} /></div>
+                <div className="icon"> <MailOutlineIcon sx={{ fontSize: 40 }} /></div>
+                </div>
+               
 
             </section>
         </div>
